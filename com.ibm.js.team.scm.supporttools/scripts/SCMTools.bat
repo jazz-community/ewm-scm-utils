758573c13766facddbc1c9c43427af6ab6d42ad3
Ralph Schoon <ralph.schoon@de.ibm.com>
2019-11-26 08:35:56 +0100

    Package

--- com.ibm.js.team.scm.supporttools/scripts/SCMTools.bat
+++ com.ibm.js.team.scm.supporttools/scripts/SCMTools.bat
@@ -1,7 +1,8 @@
 @echo off
 rem set JAVA_HOME=C:\PROGRA~1\Java\jre1.8.0_181
 rem set JAVA_HOME=C:\Program Files (x86)\Java\jre1.8.0_191
-set PLAIN_JAVA=C:\RTC606Dev\Installs\PlainJavaAPI
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_231
+set PLAIN_JAVA=C:\RTC6011Dev\installs\PlainJavaAPI
 
-# In the -Djava.ext.dirs always keep the Java distribution in the front
-"%JAVA_HOME%\bin\java" -Djava.ext.dirs="%JAVA_HOME%/lib/ext;%JAVA_HOME%/jre/lib/ext;%PLAIN_JAVA%" -cp "./scmtools_lib;%PLAIN_JAVA%;" -jar scmtools.jar %*
+rem In the -Djava.ext.dirs always keep the Java distribution in the front
+"%JAVA_HOME%\bin\java" -Djava.ext.dirs="%JAVA_HOME%/lib/ext;%JAVA_HOME%/jre/lib/ext;%PLAIN_JAVA%" -cp "./SCMTools_lib;%PLAIN_JAVA%;" -jar SCMTools.jar %*
--- com.ibm.js.team.scm.supporttools/scripts/SCMTools.sh
+++ com.ibm.js.team.scm.supporttools/scripts/SCMTools.sh
@@ -6,4 +6,4 @@ PLAIN_JAVA=/usr/RTC606Dev/Installs/PlainJavaAPI
 export PLAIN_JAVA
 
 # In the -Djava.ext.dirs always keep the Java distribution in the front
-$JAVA_HOME/bin/java -Djava.ext.dirs=$JAVA_HOME/lib/ext:$JAVA_HOME/jre/lib/ext:$PLAIN_JAVA -cp ./scmtools_lib:$PLAIN_JAVA%:./lib: -jar scmtools.jar "$@"
+$JAVA_HOME/bin/java -Djava.ext.dirs=$JAVA_HOME/lib/ext:$JAVA_HOME/jre/lib/ext:$PLAIN_JAVA -cp ./SCMTools_lib:$PLAIN_JAVA%:./lib: -jar SCMTools.jar "$@"
