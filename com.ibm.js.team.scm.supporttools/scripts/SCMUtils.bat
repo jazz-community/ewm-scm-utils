1c9ace2f6e4eda5210097391350508eb67bb32ad
Ralph Schoon <ralph.schoon@de.ibm.com>
2020-07-24 13:04:50 +0200

    Update SCMUtils.bat

--- com.ibm.js.team.scm.supporttools/scripts/SCMUtils.bat
+++ com.ibm.js.team.scm.supporttools/scripts/SCMUtils.bat
@@ -2,7 +2,7 @@
 rem set JAVA_HOME=C:\PROGRA~1\Java\jre1.8.0_181
 rem set JAVA_HOME=C:\Program Files (x86)\Java\jre1.8.0_191
 set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_231
-set PLAIN_JAVA=C:\RTC6011Dev\installs\PlainJavaAPI
+set PLAIN_JAVA=C:\RTC6061Dev\installs\PlainJavaAPI
 
 rem In the -Djava.ext.dirs always keep the Java distribution in the front
 "%JAVA_HOME%\bin\java" -Djava.ext.dirs="%JAVA_HOME%/lib/ext;%JAVA_HOME%/jre/lib/ext;%PLAIN_JAVA%" -cp "./SCMUtils_lib;%PLAIN_JAVA%;" -jar SCMUtils.jar %*
